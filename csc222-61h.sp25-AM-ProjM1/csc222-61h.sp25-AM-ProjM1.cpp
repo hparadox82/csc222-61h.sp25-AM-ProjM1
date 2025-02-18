84d0167d190a89a15c43531ac64dc49fd6ac076c
me <yuh@bruh.com>
2025-02-17 21:32:02 -0500

    Add project files.

---
+++ csc222-61h.sp25-AM-ProjM1/csc222-61h.sp25-AM-ProjM1.cpp
@@ -0,0 +1,11 @@
+// csc222-61h.sp25-AM-ProjM1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
